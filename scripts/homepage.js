04676c628810b0131a86c0f20eff73c3f592831e
Daniel Tang <danieltang68372@gmail.com>
2018-11-01 13:50:17 +0100

    Added header bg image, homepage js file,

---
+++ scripts/homepage.js
@@ -0,0 +1,8 @@
+var placeholdertext = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Morbi tristique senectus et netus et malesuada fames ac. Fermentum posuere urna nec tincidunt praesent. Nisi scelerisque eu ultrices vitae auctor eu. Amet massa vitae tortor condimentum. Vulputate dignissim suspendisse in est. Urna nec tincidunt praesent semper feugiat nibh sed pulvinar. Ipsum a arcu cursus vitae congue mauris rhoncus aenean.";
+
+var element = document.getElementsByClassName("panel");
+
+Array.prototype.forEach.call(element, function(e) {
+  	e.getElementsByClassName("d-flex")[0].innerHTML = placeholdertext;
+});
+

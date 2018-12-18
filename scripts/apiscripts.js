bb43a7bfe3dbdbf85c843b6a896009358ffb642b
MNalbant <m.nalbant042@gmail.com>
2018-12-18 12:02:34 +0100

    laptop

--- scripts/apiscripts.js
+++ scripts/apiscripts.js
@@ -1,24 +1,24 @@
 function func1() {
 	var user = {
-		FirstName : $( "#FirstName" ).val(),
-		LastName : $( "#LastName" ).val(),
-		Mail : $( "#Email" ).val(),
-		Password : $( "#Password" ).val(),
+		FirstName : $("#FirstName").val(),
+		LastName = $("#LastName").val(),
+		Mail = $("#Email").val(),
+		Password = $("#Password").val()
 	};
-var l = JSON.stringify(user);
 
-	fetch('https://localhost:44386/api/users', {  
-    method: 'POST',
-		mode: "no-cors",  
-    headers: {  
-     'Content-Type': 'application/json'
-    },  
-     body: l
-})
-.then(function (data) {  
-  console.log('Request success: ', data);  
-})  
-.catch(function (error) {  
-  console.log('Request failure: ', error);  
-});
-};
+	var userstring = JSON.stringify(user);
+	fetch('https://localhost:44386/api/users', {
+		method: 'post',
+		headers: {
+		  "Content-type": "application/x-www-form-urlencoded; charset=UTF-8"
+		},
+		body: userstring
+	  })
+	  .then(json)
+	  .then(function (data) {
+		console.log('Request succeeded with JSON response', data);
+	  })
+	  .catch(function (error) {
+		console.log('Request failed', error);
+	  });
+  }

df9850a447e8752abfee641fac86535e10ca7c1a
MNalbant <m.nalbant042@gmail.com>
2019-01-07 13:55:43 +0100

    save

--- scripts/apiscripts.js
+++ scripts/apiscripts.js
@@ -1,24 +1,26 @@
 function func1() {
 	var user = {
-		FirstName : $("#FirstName").val(),
-		LastName = $("#LastName").val(),
-		Mail = $("#Email").val(),
-		Password = $("#Password").val()
+			FirstName: $("#FirstName").val(),
+			LastName: $("#LastName").val(),
+			Mail: $("#Email").val(),
+			Password: $("#Password").val()
+	};
+	var l = JSON.stringify(user);
+
+	var settings = {
+			"async": true,
+			"crossDomain": true,
+			"url": "https://localhost:44387/api/users",
+			"method": "POST",
+			"headers": {
+					"Content-Type": "application/json",
+					"cache-control": "no-cache"
+			},
+			"processData": false,
+			"data": l
 	};
 
-	var userstring = JSON.stringify(user);
-	fetch('https://localhost:44386/api/users', {
-		method: 'post',
-		headers: {
-		  "Content-type": "application/x-www-form-urlencoded; charset=UTF-8"
-		},
-		body: userstring
-	  })
-	  .then(json)
-	  .then(function (data) {
-		console.log('Request succeeded with JSON response', data);
-	  })
-	  .catch(function (error) {
-		console.log('Request failed', error);
-	  });
-  }
+	$.ajax(settings).done(function (response) {
+			console.log(response);
+	});
+}
